3b1e60dfab9be4731855581b0619ee467e372ac0
Michel IBRAHIM <46622099+michel21ibrahim@users.noreply.github.com>
2021-05-24 20:17:15 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,27 @@
+
+/**
+ * Décrivez votre classe Test ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Test extends user
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private int x;
+
+    /**
+     * Constructeur d'objets de classe Test
+     */
+    public Test()
+    {
+        // initialisation des variables d'instance
+        x = 0;
+    }
+
+  public static void main(String[] args) {
+    String chaine1 = "bonjour";
+  
+    System.out.println("chaine 1 :"+ chaine1) ;
+  }
+}
